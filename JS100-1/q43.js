e4ccbc5d013043f31ec447e243b15e25fffa23bd
Jiyeon Kim <cleo0718@gmail.com>
2022-02-24 19:23:58 +0900

    js

---
+++ JS100-1/q43.js
@@ -0,0 +1,25 @@
+//43. 10진수를 2진수로
+//숫자를 입력받고 이를 2진수로 바꾸어서 출력하시오
+
+let num = '';
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question('숫자입력: ', (num) => {
+  let arr = [];
+  let output = "";
+  while (num) {
+    arr.push(num % 2);
+    num = parseInt(num / 2, 10)
+  }
+  arr.reverse();
+  arr.forEach((num) => {
+    output += num;
+  })
+  console.log(output);
+  rl.close();
+});
